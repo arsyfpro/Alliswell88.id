a8b7c1bba622186d73158b10c28f09e001cd35c7
fadiamaswin <66837982+fadiamaswin@users.noreply.github.com>
2020-12-20 00:58:07 +0700

    add produk.php dan detail.php

---
+++ detail.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DETAIL PER PRODUK</title>
+	<meta charset="utf-8">
+    <?php include 'scrsty.php'; ?>
+</head>
+<body>
+
+	<?php include 'navbar.php'; ?>
+
+
+	<div class="card-visible" style="width: 75%; margin: 50px 100px 0px 100px;">
+  		<div class="row no-gutters ">
+    		<div class="col-md-4">
+  				<img src="image/christmas-scrunchie.png" class="card-img-top" style="border-radius: 30px; margin: 30px 0px 0px 30px" align="center">
+  			</div>
+  			<div class="col-md-8">
+  				<div class="card-body" style="margin-left: 150px; width: 100%;">
+    				<h5 class="card-text">nama</h5>
+    				<h5 class="card-text">harga</h5>
+    				<br><br>
+    				<a href="keranjang.php" class="btn btn-sm btn-danger" style="width: 100%">Add To Chart</a>
+    				<br><br>
+    				<p style="text-align: justify;">Deskripsi Masker 3 Ply 3Ply isi 2 Box isi100 Pcs Earloop 3 lapis Face Mask. Sablon di dus tidak selalu sama ya. Masker Untuk umum Non Medis Filter 3 Ply. Terdapat aluminum sheet yang bisa diatur sesuai bentuk hidung Anda. Masker 3 ply standart internasional. Masker 3 ply (terdiri dari 3 lapisan, lapisan luar, dalam dan bagian tengah yang berfungsi sebagai filter, bactery filter efficiency 99.9%) dengan tali karet lentur yang dipasang di telinga. Menutup hidup dan mulut dengan sempurna, dan tahan percikan air. Cara pakai mudah dengan menggantungkan tali karet di kedua telinga dan menarik bagian bawah masker hingga menutup sampai dagu. Masker Untuk umum Non Medis Filter 3 Ply. Terdapat aluminum sheet yang bisa diatur sesuai bentuk hidung Anda. Masker 3 ply standart internasional. Masker 3 ply (terdiri dari 3 lapisan, lapisan luar, dalam dan bagian tengah yang berfungsi sebagai filter, bactery filter efficiency 99.9%) dengan tali karet lentur yang dipasang di telinga. Menutup hidup dan mulut dengan sempurna, dan tahan percikan air. Cara pakai mudah dengan menggantungkan tali karet di kedua telinga dan menarik bagian bawah masker hingga menutup sampai dagu.</p>
+  				</div>
+  			</div>			
+  		</div>
+	<br><br>
+	</div>
+
+	<?php include 'footer.php'; ?>
+
+</body>
+</html>
---
+++ produk.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PRODUK</title>
+	<meta charset="utf-8">
+    <?php include 'scrsty.php'; ?>
+</head>
+<body>
+
+	<?php include 'navbar.php'; ?>
+
+<div class="card" align="center">
+  <div class="card-body">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #48695a">Edisi</a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="3">List Edisi</a>
+      </div>
+  </div>
+</div>
+
+<div class="container mt-5">
+    
+<div class="row justify-content-center">
+	<a href="detail.php" style="color: inherit; text-decoration: none;">
+  <figure class="card card-product">
+    <div class="img-wrap"><img src="image/christmas-scrunchie.png"></div>
+    <figcaption class="info-wrap">
+        <h4 class="title">Christmas Scrunchie</h4>
+        <p class="desc">Desainnya indah, seindah hari natalmu!</p>
+        <div class="rating-wrap">	
+          <div class="label-rating">132 reviews</div>
+          <div class="label-rating">154 orders </div>
+        </div> <!-- rating-wrap.// -->
+    </figcaption>
+    <div class="bottom-wrap">
+      <a href="keranjang.php" class="btn btn-sm btn-danger active float-right">Add To Chart</a> 
+      <div class="price-wrap h5 float-left">
+        <span class="price-new">Rp.12.000</span> <del class="price-old">Rp14.000</del>
+      </div> <!-- price-wrap.// -->
+    </div> <!-- bottom-wrap.// -->
+  </figure></a>
+</div>
+
+</div> 
+<!--container.//-->
+
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
