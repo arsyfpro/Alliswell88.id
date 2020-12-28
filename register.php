eede4afd4298af03ae4cd4fcefdd8cbd7666d935
arsyfpro <udonorewakoneko@gmail.com>
2020-12-29 02:05:05 +0700

    belum fix, script untuk registrasi akun

--- register.php
+++ register.php
@@ -10,37 +10,60 @@ include 'koneksi.php';
     <meta charset="utf-8" />
     <?php include 'scrsty.php'; ?>
     <title>Masker & Scrunchies</title>
+
+    <style type="text/css">
+      .btn-kustom {
+        color: #fff;
+        background-color: #5d8975;
+        border-color: #5d8975;
+      }
+
+      .btn-kustom:hover {
+        color: #fff;
+        background-color: #48695a;
+        border-color: #48695a;
+      }
+    </style>
 </head>
 <body>
 <?php include "navbar.php";?>
  </div>
-  <div class="container">
-  <form action="#" class="login_design">
+  <div style="color: #48695a; width: 600px;" class="container">
+  <form method="post" class="login_design">
     <br> <br>
-    <h2>Sign Up</h2><br>
+
+    <h2 align="center">Sign Up</h2><br>
+    <p align="center">for your alliswell88.id account</p>
+    <br><br>
+
+    <?php
+      if (isset($_POST['cekemail'])) {
+        $emailregis = $_POST['regisemail'];
+
+        $cekakun = $koneksi->query("SELECT email FROM akun WHERE email = '$emailregis'");
+        $adadata = $cekakun->num_rows;
+
+        if ($adadata > 0) {
+          echo "<div class='alert alert-danger' role='alert'>Email telah digunakan, silakan gunakan email lain!</div>";
+        }
+        else{
+          echo "<script> window.location.href = 'register_lanjut.php?email=".$emailregis."'; </script>";
+        }
+      }
+    ?>
+
     <div class="form-group justify-content-center">
-      <label for="inputName">Name</label>
-       <input type="text" class="form-control" id="inputName" placeholder="Full name">
-     </div>
-    <div class="form-group justify-content-center">
-      <label for="inputEmail3">Email</label>
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Email" required="">
+      <label for="inputEmail3">Input your email first!</label>
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="regisemail" required>
     </div>
-    <div class="form-group">
-    <label for="inputPassword3">Password</label>
-    <input type="password" class="form-control" id="inputPassword3" placeholder="Password" required="">
-    </div>
-    <div class="form-group">
-    <label for="inputVerify3">Verify</label>
-    <input type="password" class="form-control" id="inputVerify3" placeholder="Password (again)" required="">
-    </div>
-    <div class="form-group">
-    <button type="submit" class="btn btn-success btn-lg float-right">Register</button>
+    <br>
+    <div align="center" class="form-group">
+    <button type="submit" name="cekemail" class="btn btn-kustom">Lanjut</button>
     </div> 
-</div>
+  </form>
+  </div>
 
-</form>
-
+<br>
 <br>
 <br>
 
