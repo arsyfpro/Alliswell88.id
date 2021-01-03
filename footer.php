fab82021e72be2072421cfdeb47147f9581ffd0b
putriidaulayy <annisaputridaulay@gmail.com>
2021-01-04 02:03:08 +0700

    add footer

--- footer.php
+++ footer.php
@@ -7,53 +7,45 @@
   <section id="footer">
     <div class="container">
       <div class="row text-center text-xs-center text-sm-left text-md-left">
-        <div class="col-xs-12 col-sm-4 col-md-4">
-          <h5>Quick links</h5>
-          <ul class="list-unstyled quick-links">
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+        <div class="col-xs-14 col-sm-5 col-md-4">
+          <<img src="icon/logoputih.png" height="30">
+          <br><br>
+            <p style="color:white;">Adalah sebuah online shop yang bergerak dibidang produksi
+            masker dan srunchie sejak tahun 2018. Kualitas adalah prioritas kami dengan harga yang terjangkau.</p>
+          
           </ul>
         </div>
-        <div class="col-xs-12 col-sm-4 col-md-4">
+        <div class="col-xs-14 col-sm-6 col-md-4">
           <h5>Quick links</h5>
           <ul class="list-unstyled quick-links">
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+            <li><a href="search.php"></i>Search</a></li>
+            <li><a href="allstuff.php"></i>Shop All</a></li>
+            <li><a href="http://localhost/tubesIMK/edisi.php?item=1"></i>Masker</a></li>
+            <li><a href="http://localhost/tubesIMK/edisi.php?item=2"></i>Scrunchie</a></li>
+            
           </ul>
         </div>
-        <div class="col-xs-12 col-sm-4 col-md-4">
-          <h5>Quick links</h5>
+        <div class="col-xs-14 col-sm-4 col-md-4">
+          <h5>Contact Us</h5>
           <ul class="list-unstyled quick-links">
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-            <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-            <li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+            <p style="color:white;"><i class="fa fa-location-arrow"></i> Jl. Ksatria No.25, Satria, Kec. Binjai Kota, Kota Binjai, Sumatera Utara 20714 </p>
+            <p style="color:white;"><i class="fa fa-phone"></i>  +62 882-6638-8888  </p>
+            <p style="color:white;"><i class="fa fa fa-envelope"></i> Surbaktigabriella@gmail.com  </p>
           </ul>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
           <ul class="list-unstyled list-inline social text-center quick-links">
-            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
-            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
-            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
-            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
+            <li class="list-inline-item"><a href="https://www.instagram.com/alliswell88.id/"><img src="icon/instagram.png" height="40"></i></a></li>
+            <li class="list-inline-item"><a href="https://shopee.co.id/alliswell88.id" target="_blank"><img src="icon/shopee.png" height="40"></a></li>
           </ul>
         </div>
         <hr>
       </div>  
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-          <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
-          <p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Alliswell88.id</a></p>
+          <p style="font-size: 15px;">©All Right Reserved <a class="text-green ml-2" href="index.php" target="_blank">Alliswell88.id</a></p>
         </div>
         <hr>
       </div>  
